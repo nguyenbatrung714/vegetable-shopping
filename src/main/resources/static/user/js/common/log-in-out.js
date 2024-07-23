482df8de8ee936b07d5ee70ce8b631c054c25224
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-07-24 01:30:42 +0700

    log out

---
+++ src/main/resources/static/user/js/common/log-in-out.js
@@ -0,0 +1,25 @@
+function checkLogin1() {
+    let checkValue = sessionStorage.getItem('token');
+    $('#userMenuContent').empty();
+    if (checkValue == null) {
+        $('#userMenuContent').append(`
+                <li><a href="/vegetable-shopping/login">Login</a></li>
+                <li><a href="/vegetable-shopping/register">Register</a></li>
+        `)
+    } else {
+        $('#userMenuContent').append(`
+                <li><a href="/vegetable-shopping/orders">Your orders</a></li>
+                <li><a href="/vegetable-shopping/update-account">Update account</a></li>
+                <li><a href="/vegetable-shopping/change-password">Change password</a></li>
+                <li><a onclick="logout()">Log out</a></li>
+        `)
+    }
+}
+
+function logout() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('userName');
+    window.location.href = "/vegetable-shopping/home";
+}
+
+window.checkLogin1();
